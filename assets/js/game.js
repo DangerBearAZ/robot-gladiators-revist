79d0b3d2f5d6b99af2c80dd77dd29f4dd48b6d4b
claire ogesen <claire.ogesen.co@gmail.com>
2021-07-21 18:02:57 -0700

    initiated

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-alert("this is an alert!");
+// fight 
+
+var playerName = prompt("what is your robot's name?");
+
+console.log(playerName); 
+console.log("This logs a string good for leaving yourself a message");
+console.log(10+10);
+console.log("Our robot's name is " + playerName);
+
+function fight () {
+    alert('the fight has begun!')
+}
+
+// fight();
